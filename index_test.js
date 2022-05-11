fbeb3b8e4c087587918bf5c5561bc5207938b551
AngelxLucero <102089892+AngelxLucero@users.noreply.github.com>
2022-05-11 10:07:48 -0600

    Added Card Value
    Was able to value to J,Q,K,A

--- index_test.js
+++ index_test.js
@@ -1,19 +1,19 @@
-var expect = chai.expect;
+// var expect = chai.expect;
 
-describe("MyFunctions", function(){
-    describe("#doSomething", function() {
-        it('should concatenate the two parameters',function() {
-            var x = doSomething("Hello", 5);
-            expect(x).to.equal("Hello5");
-        });
+// describe("MyFunctions", function(){
+//     describe("#doSomething", function() {
+//         it('should concatenate the two parameters',function() {
+//             var x = doSomething("Hello", 5);
+//             expect(x).to.equal("Hello5");
+//         });
 
-        it('should throw an error if first parameter is not a string', function(){
-            expect(function(){
-                doSomething(5, 5);
-            }).to.throw(Error);
-        });
-    });
-});
+//         it('should throw an error if first parameter is not a string', function(){
+//             expect(function(){
+//                 doSomething(5, 5);
+//             }).to.throw(Error);
+//         });
+//     });
+// });
 
 
 
--- week-6.js
+++ week-6.js
@@ -26,7 +26,21 @@ class Card {
      function createDeck(){
         var suits = ["♠", "♣", "♥", "♦"];
         var value = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
-        //var faceValue = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+         Card_Value_Map = {
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            '10': 10,
+            'J': 11,
+            'Q': 12,
+            'K': 13,
+            'A': 14,
+        }
         var deck = []
     for(let suitCounter = 0; suitCounter < 4; suitCounter++){
         for(let valueCounter = 0; valueCounter < 13; valueCounter++){
@@ -35,6 +49,14 @@ class Card {
         }
         return deck;
     }
+
+// if('J' > 'K'){
+//     console.log('Jack wins!');
+// }else{
+//     console.log('King Wins!');
+// }
+
+
 
 //create a deck that contains 52 cards with numbers and suits
 
